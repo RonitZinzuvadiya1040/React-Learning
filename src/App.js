42b7dd4a3cbb9364e1aa7ac8b350325af61b2f35
Ronit Zinzuvadiya <ronitzinzuvadiya2001@gmail.com>
2026-05-15 14:37:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>hello world</p>
+        <h2>Welcome to my first React app!</h2>
+        <h3>This is a simple example to get started with React.</h3>
+        <h4>Enjoy building your React applications!</h4>
+        <h5>Happy coding!</h5>
+        <h6>test</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
